64be4f67523769a38822229dbb6f48562c3d2b82
Chris Moorhead <creativebrewdev@gmail.com>
2017-06-12 18:20:39 -0500

    port change

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ io.on('connection', function(socket){
     })
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+app.set('port', ( process.env.PORT || 3000));
+
+http.listen(app.get('port'), function(){
+    console.log('listening on *:' + app.get('port'));
 });
