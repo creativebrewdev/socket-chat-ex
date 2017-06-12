adf712a591106b8c4ebd54e0f55aab2bf92e6a9f
Chris Moorhead <creativebrewdev@gmail.com>
2017-06-12 13:19:38 -0500

    added server msg functionality

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/public/views/index.html');
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+    });
+});
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
